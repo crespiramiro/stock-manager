216c8a447080312537355b38fd870b0ca9089295
crespiramiro <crespiramiro@outlook.com>
2024-07-01 23:09:02 -0300

    .

--- frontend/stock-manager/src/app/inventory/assets/EditProduct.jsx
+++ frontend/stock-manager/src/app/inventory/assets/EditProduct.jsx
@@ -3,59 +3,24 @@ import { useEffect, useState } from "react";
 import { ModalComponent } from "./ModalComponent";
 
 const EditProductModal = ({ product, onSave, onClose }) => {
-  const [formData, setFormData] = useState({
-    name: "",
-    price: "",
-    quantity: "",
-    category: "",
-    updatedAt: "",
-  });
+  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    if (product) {
-      setFormData({
-        name: product.name,
-        price: product.price,
-        quantity: product.quantity,
-        category: product.category,
-        updatedAt: product.updatedAt,
-      });
-    }
-  }, [product]);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const openModal = () => {
+    setIsOpen(true);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(`http://localhost:8080/api/products/${product}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to update the product');
-      }
-
-      onSave();
-      onClose();
-    } catch (error) {
-      console.error('Error updating the product:', error);
-      alert('Failed to update the product');
-    }
+  const closeModal = () => {
+    setIsOpen(false);
   };
+
 
   return (
-   <ModalComponent />
+    <ModalComponent
+        product={product}
+        onSave={onSave}
+        onClose={closeModal}
+        isOpen={isOpen}
+      />
   );
 };
 
