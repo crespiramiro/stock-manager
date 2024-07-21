ed4ea5689674020d7212e320ef3de7db9dc6b89e
crespiramiro <crespiramiro@outlook.com>
2024-07-21 14:17:23 -0300

    sidebar responsive

--- frontend/stock-manager/src/app/components/auth/AuthPage.jsx
+++ frontend/stock-manager/src/app/components/auth/AuthPage.jsx
@@ -13,10 +13,10 @@ export default function AuthPage() {
 
     return (
         <section className="flex w-screen">
-            <section className="w-[45%] bg-black">
+            <section className="md:w-[45%] w-0 bg-black">
                 {/* Aquí puedes agregar la animación si es necesario */}
             </section>
-            <section className="w-[55%] flex flex-col items-center justify-center bg-white px-16 ">
+            <section className="md:w-[55%] w-screen flex flex-col items-center justify-center bg-white px-16 ">
                 <button
                     className="absolute top-8 right-16 py-2 px-3 font-medium rounded-md"
                     onClick={() => setShowLogin(!showLogin)}
--- frontend/stock-manager/src/app/dashboard/Slidebar.jsx
+++ frontend/stock-manager/src/app/dashboard/Slidebar.jsx
@@ -1,4 +1,6 @@
 'use client'
+import { ArrowLeft, List } from "phosphor-react"
+import { useState } from "react"
 
 export default function Slidebar (){
 
@@ -10,17 +12,25 @@ export default function Slidebar (){
         window.location.href='/'
     }
 
-    return (
+    const [isOpen, setIsOpen] = useState(false);
 
-        <section className="flex flex-col gap-y-12 h-screen w-3/12 bg-black text-[#f5f5f5] px-12 py-8  " >
-            <h2 className="font-bold text-4xl  p-4 self-center " >Stock Management</h2>
-            <div className=" flex flex-col h-full justify-between " >
-                <ul className="flex flex-col font-semibold gap-y-8 items-start px-4 " >
-                    <li className="text-2xl hover:cursor-pointer "  onClick={handleHome} >Home</li>
-                    <li className="text-2xl hover:cursor-pointer " onClick={handleInventory} >Inventory</li>
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <section className={`flex flex-col gap-y-12 h-screen ${isOpen ? 'w-1/2 md:w-1/3 ' : 'w-3/12 md:w-2/12 '} text-center bg-black text-[#f5f5f5] px-4 md:px-12 py-8 transition-all duration-300`}>
+            <button className={`${isOpen ? 'self-end' : 'self-center' }`} onClick={toggleMenu}>
+                    {isOpen ? <ArrowLeft size={32} /> : <List size={32} />}
+                </button>
+                <h2 className={`font-semibold ${isOpen ? 'text-2xl md:text-4xl text-center self-center text-wrap ' : 'hidden'} py-4 px-8 `}>Stock Management</h2>
+            <div className="flex flex-col h-full justify-between">
+                <ul className={`flex flex-col font-semibold gap-y-8 ${isOpen ? 'items-center' : 'items-center'}`}>
+                    <li className={`${isOpen ? 'text-2xl' : 'hidden'} hover:cursor-pointer`} onClick={handleHome}>Home</li>
+                    <li className={`${isOpen ? 'text-2xl' : 'hidden'} hover:cursor-pointer`} onClick={handleInventory}>Inventory</li>
                 </ul>
-                <ul className="flex flex-col font-semibold items-start px-4 py-8" >
-                <li className="text-2xl" >Help</li>
+                <ul className={`flex flex-col font-semibold ${isOpen ? 'items-center' : 'items-center'} py-8`}>
+                    <li className={`${isOpen ? 'text-2xl' : 'hidden'}`}>Help</li>
                 </ul>
             </div>
         </section>
