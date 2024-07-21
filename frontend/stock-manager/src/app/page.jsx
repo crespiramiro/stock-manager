9d968f05639ec41a75f563b8126be48e9cf294b1
crespiramiro <crespiramiro@outlook.com>
2024-07-21 16:32:18 -0300

    inventory overflow fix

--- frontend/stock-manager/src/app/page.jsx
+++ frontend/stock-manager/src/app/page.jsx
@@ -5,7 +5,7 @@ import AuthPage from "./components/auth/AuthPage";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-screen " >
+    <main className="flex min-h-screen w-screen md:overflow-hidden " >
          <AuthPage /> 
     </main>
   );
