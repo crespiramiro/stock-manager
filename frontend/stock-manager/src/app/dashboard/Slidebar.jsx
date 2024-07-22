f00a179e7ceefc3fb2d626120d7a5aaf67c9eae8
crespiramiro <crespiramiro@outlook.com>
2024-07-21 21:16:58 -0300

    slidebar ui

--- frontend/stock-manager/src/app/dashboard/Slidebar.jsx
+++ frontend/stock-manager/src/app/dashboard/Slidebar.jsx
@@ -1,9 +1,25 @@
 'use client'
 import Link from "next/link"
-import { ArrowLeft, List } from "phosphor-react"
-import { useState } from "react"
+import { ArrowLeft, List, UserCircle, CaretUp } from "phosphor-react"
+import { useState, useEffect } from "react"
+import { jwtDecode } from "jwt-decode"
 
 export default function Slidebar (){
+
+    const [userName, setUserName] = useState('')
+    const [userEmail, setUserEmail] = useState('')
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          try {
+            const decodedToken = jwtDecode(token);
+            setUserName(decodedToken.userName); 
+            setUserEmail(decodedToken.userEmail)
+          } catch (error) {
+          }
+        }
+      }, []);
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -16,15 +32,19 @@ export default function Slidebar (){
             <button className={`${isOpen ? 'self-end' : 'self-center' }`} onClick={toggleMenu}>
                     {isOpen ? <ArrowLeft size={32} /> : <List size={32} />}
                 </button>
-                <h2 className={`font-semibold ${isOpen ? 'text-2xl md:text-4xl text-center self-center text-wrap ' : 'hidden'} py-4 px-8 `}>Stock Management</h2>
+                <h2 className={`font-semibold ${isOpen ? 'text-xl md:text-3xl text-center self-center text-wrap ' : 'hidden'} py-4 px-8 `}>Stock Management</h2>
             <div className="flex flex-col h-full justify-between">
                 <ul className={`flex flex-col font-semibold gap-y-8 ${isOpen ? 'items-center' : 'items-center'}`}>
-                    <Link href={"/dashboard"} className={`${isOpen ? 'text-2xl' : 'hidden'} hover:cursor-pointer`}>Home</Link>
-                    <Link href={"/inventory"} className={`${isOpen ? 'text-2xl' : 'hidden'} hover:cursor-pointer`}>Inventory</Link>
+                    <Link href={"/dashboard"} className={`${isOpen ? 'text-xl' : 'hidden'} hover:cursor-pointer`}>Home</Link>
+                    <Link href={"/inventory"} className={`${isOpen ? 'text-xl' : 'hidden'} hover:cursor-pointer`}>Inventory</Link>
                 </ul>
-                <ul className={`flex flex-col font-semibold ${isOpen ? 'items-center' : 'items-center'} py-8`}>
-                    <li className={`${isOpen ? 'text-2xl' : 'hidden'}`}>Help</li>
-                </ul>
+               <div className="flex flex-row items-center justify-start gap-x-4 " >
+               <UserCircle size={44} color="#ffffff" />
+               <div className={`${isOpen ? 'flex flex-col items-start justify-center' : 'hidden'}`}>
+               <h2 className="font-semibold text-lg " >Hi, {userName || 'User'}</h2>
+               <h3 className=" text-md text-gray-500" >{userEmail || 'Mail' }</h3>
+               </div>
+               </div>
             </div>
         </section>
 
